993ef336821a7091031f3a472308dc87b2490cf3
NetSecGirl <31288834+NetSecGirl@users.noreply.github.com>
2018-02-03 15:04:22 -0800

    Create deletenode.c
    1.create list
    2.push to head
    3.add to tail
    4.Delete node from list (copy data from the next node to the node to be deleted)

---
+++ deletenode.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node {
+    int val;
+    struct node* next;
+};
+
+void deleteNode(struct node* node) {
+    struct node* temp;
+    temp = node;
+    node->val = temp->next->val;
+    if(temp->next->next == NULL)
+        node->next = NULL;
+    else
+        node->next = temp->next->next;
+    
+}
+struct node* Build()
+{
+    struct node* head;
+    struct node* two;
+    head = malloc(sizeof(struct node));
+    two = malloc(sizeof(struct node));
+    head->val = 2;
+    head->next = two;
+    two->val = 3;
+    two->next = NULL;
+    return head;
+}
+int main()
+{
+    struct node* head = Build();
+    struct node* newNode;
+    struct node* tail;
+    struct node* temp;
+    int count = 0;
+    newNode = malloc(sizeof(struct node));
+    tail = malloc(sizeof(struct node));
+    
+    /* Push node to head*/
+    newNode->val = 1;
+    newNode->next = head;
+    
+    head = newNode;
+    
+    /*push node to tail */
+    tail->val = 4;
+    tail->next = NULL;
+    temp = head;
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = tail;
+    
+    deleteNode(head);
+    temp = head;
+    while(temp !=NULL) {
+        printf("value = %d\n", temp->val);
+        temp = temp->next;
+        count ++;
+    }
+        
+    printf("Count = %d", count);
+    return 0;
+}
